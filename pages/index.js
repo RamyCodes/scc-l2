b9452c462909426f5876d57e63b0fb69bc5af68b
RamyCodes <87505205+RamyCodes@users.noreply.github.com>
2023-02-25 01:25:41 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const Index = () => (
   <div className="mt-8 max-w-xl mx-auto px-8">
     <h1 className="text-center">
       <span className="block text-xl text-gray-600 leading-tight">
-        Ramy Naccache - 2001529 welcomes you to <span className="block text-xl text-white-600 leading-tight"> Chilli's </span>
+        Ramy Naccache - 2001529 welcomes you to
       </span>
       <span className="block text-5xl font-bold leading-none">
         Cloud Computing Course
